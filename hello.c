d6a77ab99f93ae63de416092e2d293b48e81ecdf
Minh Tran <mtran33@huskers.unl.edu>
2025-09-01 20:51:45 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Minh Tran
+ *         mtran33@huskers.unl.edu
+ * Date: 2025-09-01
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Minh Tran !!1!1!\n");
+  printf("My major is Computer Engineering.\n");
+
+  return 0;
+}
